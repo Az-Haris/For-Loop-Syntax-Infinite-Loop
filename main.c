f75280df5b240b8472d5f08e7996f52b8de1563e
MD Alimuzzaman Haris <73691596+Az-Haris@users.noreply.github.com>
2022-01-25 17:06:31 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int a;
+
+    for(a=1; a>0; a++)
+    {
+        printf("\t Az Haris %d\n", a);
+    }
+
+    getch();
+    return 0;
+}
+
+/*
+    For Loop Syntax :
+
+    for(initialization; test condition; increment/decrement)
+    {
+        statement 1;
+        statement 2;
+        statement 3;
+        statement 4;
+        statement 5;
+        ...........;
+        ...........;
+        statement n;
+    }
+
+*/
